7b5864477fc09482dc0fa2fc9806b0c97fd3562d
DellZHackintosh <77269678+DellZHackintosh@users.noreply.github.com>
2024-08-28 16:23:20 +0800

    Canary

--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,4 @@
-# Fluent-Flarum
-
-简体中文 | [English](./ReadMe-en.md)
+### Fluent-Flarum
 
 使用这款主题为你的 Flarum 换新颜。
 
@@ -10,14 +8,7 @@
 
 按钮、切换开关、选项菜单、标签、边栏、模态窗口、信息栏、输入控件和复选框。
 
-另外也部分改变了 Forum Widgets、Level Ranks、FancyBox 和 Blog 的外观。
+
 
 不确定是否与 Flarum Categories 兼容。
 
-#### 展示
-
-请前往[示例站点](https://dellzhackintosh.github.io/fluent-flarum/)。
-
-### 自定义设计
-
-Fluent 主题支持进行一系列的自定义。请转到[设计指南](./documents/design.md)。
--- extend.php
+++ extend.php
@@ -1,39 +1,70 @@
 <?php
 
-namespace DaleZ\fluentflarum;
+namespace DaleZ\fluent_flarum;
 
 use Flarum\Extend;
 use Flarum\Frontend\Document;
+use Flarum\Settings\SettingsRepositoryInterface;
 
 return [
+    (new Extend\Frontend('admin'))
+        ->js(__DIR__.'/js/dist/admin.js'),
+
     (new Extend\Frontend('forum'))
         ->content(function (Document $document) {
-            $document->head[] = '<script>(function () {
-                var r = getComputedStyle(document.querySelector(":root"));
+            $settings = resolve(SettingsRepositoryInterface::class);
 
-                function changeTitleColor() {
-                    if (r.getPropertyValue("--colored-titlebar") === "false") {
+            if (!($settings->get('theme_colored_header'))) {
+                /*
+                $document->head[] = '<script>(function () {
+                    var r = getComputedStyle(document.querySelector(":root"));
+    
+                    function changeTitleColor() {
                         var m = document.querySelector(`meta[name="theme-color"]`);
                         m.content = r.getPropertyValue("--header-bg");
-                    };
-                }
-            
-                changeTitleColor();
-                if (r.getPropertyValue("--colored-titlebar") === "false") new Promise(function (resolve) {
-                    var id = setInterval(function () {
-                        if (flarum) {
-                            if (flarum.extensions) {
-                                clearInterval(id);
-                                resolve();
-                            }
-                        }
-                    }, 500)
-                }).then(function () {
-                    if (flarum.extensions["fof-nightmode"]) {
-                        document.addEventListener("fofnightmodechange", changeTitleColor);
                     }
-                });
-            })();</script>';
+                
+                    changeTitleColor();
+                    new Promise(function (resolve) {
+                        var id = setInterval(function () {
+                            if (flarum) {
+                                if (flarum.extensions) {
+                                    clearInterval(id);
+                                    resolve();
+                                }
+                            }
+                        }, 500)
+                    }).then(function () {
+                        if (flarum.extensions["fof-nightmode"]) {
+                            document.addEventListener("fofnightmodechange", changeTitleColor);
+                        }
+                    });
+                })();</script>';
+                */
+                $document->head[] = '<script>(function(){function a(){var a=document.querySelector(`meta[name="theme-color"]`);a.content=b.getPropertyValue("--header-bg")}var b=getComputedStyle(document.querySelector(":root"));a(),new Promise(function(a){var b=setInterval(function(){flarum&&flarum.extensions&&(clearInterval(b),a())},500)}).then(function(){flarum.extensions["fof-nightmode"]&&document.addEventListener("fofnightmodechange",a)})})();</script>';
+            }
+
+            switch ($settings->get('dalez-fluent-flarum.background', 'solid')) {
+                case 'solid':
+                    # code...
+                    break;
+
+                case 'mica_accent':
+                    # code...
+                    break;
+                
+                default:
+                    # code...
+                    break;
+            }
+
+            $document->foot[] = '<script>(() => {const parser = new DOMParser();const dom = parser.parseFromString(`<div id="fluent-background"><svg></svg><div></div><div></div></div>`, \'text/html\');document.body.prepend(dom.body.firstElementChild);})();</script>';
         })
-        ->css(__DIR__.'/less/forum.less'),  
+        ->css(__DIR__.'/oldless/forum.less'),  
+
+        (new Extend\Settings())
+            ->default('dalez-fluent-flarum.enableBeta', true)
+            ->default('dalez-fluent-flarum.background', 'solid'),
+
+        new Extend\Locales(__DIR__.'/locale'),
 ];
